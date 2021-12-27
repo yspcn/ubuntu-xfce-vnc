253b9e7b131b5680869ea39f59c3922b18aa4a03
yspcn <55094985+yspcn@users.noreply.github.com>
2021-12-27 23:21:12 +0800

    Update tools.sh

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -4,9 +4,9 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update 
-apt-get install debconf apt-utils -y
+apt-get install debconf apt-utils locales tzdata xfonts-wqy -y
 apt-get install -y vim wget net-tools locales bzip2 language-pack-zh-hant language-pack-gnome-zh-hant language-pack-zh-hans language-pack-gnome-zh-hans \
-    python-numpy fcitx-bin fcitx-table fcitx-googlepinyin #used for websockify/novnc
+    python-numpy ibus ibus-libpinyin ibus-table-wubi fcitx-table-wbpy fcitx-config-gtk #used for websockify/novnc
 apt-get clean -y
 
 echo "generate locales für zh_CN.UTF-8"
